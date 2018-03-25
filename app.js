91428c1a94db1c1ed2173076969d25415c0f7c40
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-03-25 14:17:32 +0300

    Use handlebars to render templates; start prepare to deploy

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+const port = process.env.port || 9000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  res.render('home.hbs', {
+    title: 'Home page',
+    body: 'Welcome to Express site!',
+  });
 });
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    title: 'About page',
+    body: 'This is a body',
+  });
 });
 app.get('/bad', (req, res) => {
   res.send({
@@ -17,4 +28,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen('9000');
+app.listen(port, () => {
+  console.log(`App is launched on port ${port}`);
+});
