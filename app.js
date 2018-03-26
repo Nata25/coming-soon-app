e78a296b16d17d7fc8e6be709d9851b8c41020bd
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-03-26 23:36:48 +0300

    App now calculates time left to a certain date

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const moment = require('moment');
 
 const app = express();
 
@@ -9,25 +10,43 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('shorten', str => str.slice(3));
+
+hbs.registerHelper('setEnding', days => {
+  if (days === 1) return `${days} day`;
+  else return `${days} days`;
+});
+
 // Routes
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    title: 'Home page',
-    body: 'Welcome to Express site!',
+    title: 'Don\'t wait too long!',
   });
 });
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    title: 'About page',
-    body: 'This is a body',
+app.get('/calculate', (req, res) => {
+  res.render('calculate.hbs', {
+    title: 'A date you can\'t wait for is ...',
   });
 });
-app.get('/bad', (req, res) => {
-  res.send({
-    message: 'Page not found'
-  });
+app.get('/result', (req, res) => {
+  const now = moment(new Date()); 
+  const end = moment(req.query.to); 
+  const duration = moment.duration(end.diff(now));
+  const days = Math.floor(duration.asDays());
+  const hours = moment().endOf('day').fromNow();
+  if (days > 0) {
+    res.render('results.hbs', {
+      title: 'Here we go!',
+      days,
+      hours,
+    });
+  } else {
+    res.render('error.hbs', {
+      title: 'Error occured!'
+    });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`App is launched on port ${port}`);
+  console.log(`App launched on port ${port}...`);
 });
